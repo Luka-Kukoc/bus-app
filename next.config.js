5c509eff47fd8374d60dc1fa52350fb02aab895c
lukakukoc <kukoc.luka@gmail.com>
2022-10-09 18:49:15 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: true,
+  },
+});
